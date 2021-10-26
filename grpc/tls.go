9a224a1212044072c0e1a542be77161cee6c6bcc
Joseph <josephmpalacio@gmail.com>
2021-10-26 16:51:38 -0500

    remove print

--- grpc/tls.go
+++ grpc/tls.go
@@ -3,7 +3,6 @@ package grpc
 import (
 	"crypto/tls"
 	"encoding/base64"
-	"fmt"
 
 	"google.golang.org/grpc/credentials"
 )
@@ -16,11 +15,6 @@ func ParseCertificates(pubCert, privCert string) (tls.Certificate, error) {
 	var certs tls.Certificate
 	var err error
 	certs, err = tls.LoadX509KeyPair(pubCert, privCert)
-	fmt.Printf("ParseCertificates err: %+v\n ", err)
-	// certPEMBlock, err := os.ReadFile(pubCert)
-	// fmt.Printf("ParseCertificates: certPEMBlock: %+v, err: %+v\n ", certPEMBlock, err)
-	// keyPEMBlock, err := os.ReadFile(privCert)
-	// fmt.Printf("ParseCertificates: keyPEMBlock: %+v, err: %+v\n ", keyPEMBlock, err)
 
 	if err != nil {
 		pubCertByte, err := base64.StdEncoding.DecodeString(pubCert)
